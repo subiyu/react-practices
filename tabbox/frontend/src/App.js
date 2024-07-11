da348de1ba2bf160ad2960fb0b47e502c9a64e34
yusubhin <sdbtnqlss@gmail.com>
2024-07-11 17:17:57 +0900

    tabbox #1 - update css
    tab list를 추가하더라도 css가 깨지지않도록 수정

--- tabbox/frontend/src/App.js
+++ tabbox/frontend/src/App.js
@@ -8,7 +8,8 @@ function App() {
         {no: 2, name: '메뉴2', active: false, contents: '메뉴2의 뷰 내용'},
         {no: 3, name: '메뉴3', active: true, contents: '메뉴3의 뷰 내용'},
         {no: 4, name: '메뉴4', active: false, contents: '메뉴4의 뷰 내용'},
-        {no: 5, name: '메뉴5', active: false, contents: '메뉴5의 뷰 내용'}
+        {no: 5, name: '메뉴5', active: false, contents: '메뉴5의 뷰 내용'},
+        {no: 5, name: '메뉴6', active: false, contents: '메뉴6의 뷰 내용'}
     ];
 
     return (
