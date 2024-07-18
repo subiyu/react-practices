47a20c4bc04ae134e6c13b744ef27e6abe57b788
yusubhin <sdbtnqlss@gmail.com>
2024-07-18 10:06:48 +0900

    emaillist backend #1

--- 03.Component/ex06/01.js
+++ 03.Component/ex06/01.js
@@ -1,19 +1,22 @@
-import { privateDecrypt } from 'crypto';
 import fs from 'fs';
 
 console.log("== Violation ===================================");
-const order = JSON.parse(fs.readFileSync('./json/data.json', 'utf-8'));
-const updateOrderProducts1 = order.products
-updateOrderProducts.push({
+let order = JSON.parse(fs.readFileSync('./json/data.json', 'utf-8'));
+const updateOrderProducts1 = order.products;
+
+updateOrderProducts1.push({
     no: 'c002-003',
     name: '블루양말',
-    amount:1
-})
+    price: 2000,
+    amount: 1
+});
 console.log(order.products, updateOrderProducts1, order.products === updateOrderProducts1);
+
 
 
 console.log("== Sol1 ========================================");
 order = JSON.parse(fs.readFileSync('./json/data.json', 'utf-8'))
+
 const updateOrderProducts2 = order.products.concat({
     no: 'c002-003',
     name: '블루양말',
@@ -24,7 +27,7 @@ console.log(order.products, updateOrderProducts2, order.products === updateOrder
 
 
 console.log("== Sol2 ========================================");
-order = JSON.parse(fs.readFileSync('./json/data.json', 'utf-8'));
+order = JSON.parse(fs.readFileSync('./json/data.json', 'utf-8'))
 
 const updateOrderProducts3 = [...order.products, {
     no: 'c002-003',
@@ -33,5 +36,3 @@ const updateOrderProducts3 = [...order.products, {
     amount: 1
 }];
 console.log(order.products, updateOrderProducts3, order.products === updateOrderProducts3);
-
-data = [{no:10, status:'Done'}, {no:20, status:'Doing'}, {no:11, status: 'Doing'}, {no:10, status:'Todo'}];
