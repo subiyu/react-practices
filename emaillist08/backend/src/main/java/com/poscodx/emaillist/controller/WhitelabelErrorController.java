d8b1f8c20a7c8052c720837a8c6063b371875397
yusubhin <sdbtnqlss@gmail.com>
2024-07-18 10:51:51 +0900

    backend emaillist #2 에러페이지 수정

--- emaillist08/backend/src/main/java/com/poscodx/emaillist/controller/WhitelabelErrorController.java
+++ emaillist08/backend/src/main/java/com/poscodx/emaillist/controller/WhitelabelErrorController.java
@@ -7,6 +7,9 @@ import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.poscodx.emaillist.dto.JsonResult;
 
 @Controller
 @RequestMapping("/error")
@@ -14,29 +17,31 @@ public class WhitelabelErrorController implements ErrorController {
 	
 	@RequestMapping("/404")
 	public String _404() {
-		return "errors/404";
+		return "index";
 	}
 	
+	@ResponseBody
 	@RequestMapping("/500")
-	public String _500() {
-		return "errors/500";
+	public JsonResult _500() {
+		return JsonResult.fail("500 INTERNAL_SERVER_ERROR");
 	}
 
+	@ResponseBody
 	@RequestMapping("")
-	public String handleError(HttpServletRequest request) {
+	public Object handleError(HttpServletRequest request) {
 	    Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
 	    
 	    if(status != null) {
 	        Integer statusCode = Integer.valueOf(status.toString());
 	    
 	        if(statusCode == HttpStatus.NOT_FOUND.value()) {
-	            return "errors/404";
+	            return "Not Found";
 	        } else if(statusCode == HttpStatus.BAD_REQUEST.value()) {
-	            return "errors/400";
+	            return JsonResult.fail("400 BAD_REQUEST");
 	        } else if(statusCode == HttpStatus.FORBIDDEN.value()) {
-	            return "errors/403";
+	            return JsonResult.fail("403 FORBIDDEN");
 	        } else if(statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
-	            return "errors/500";
+	            return JsonResult.fail("500 INTERNAL_SERVER_ERROR");
 	        } 
 	    }
 	    
--- emaillist08/backend/src/main/java/com/poscodx/emaillist/exception/GlobalExceptionHandler.java
+++ emaillist08/backend/src/main/java/com/poscodx/emaillist/exception/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.poscodx.emaillist.exception;
 
+import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 
@@ -10,27 +11,65 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.NoHandlerFoundException;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.poscodx.emaillist.dto.JsonResult;
+
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @ControllerAdvice
 public class GlobalExceptionHandler {
-	
-	
-	@ExceptionHandler(NoHandlerFoundException.class)
-	public String handlerNoHandlerFoundException(Exception e) {
-		return "index";
-	}
-	
+			
 	@ExceptionHandler(Exception.class)
-	public void handler(HttpServletRequest request, HttpServletResponse response, Exception e) throws Exception {
-		// 로깅
-		StringWriter errors = new StringWriter();
-		e.printStackTrace(new PrintWriter(errors));
-		log.error(errors.toString());
-		
-		// 에러 페이지 포워딩
-		request.setAttribute("errors", errors.toString());
-		request.getRequestDispatcher("/error/500").forward(request, response);
+	public void handler(
+			HttpServletRequest request, 
+			HttpServletResponse response,
+			Exception e
+		) throws Exception {
+			// logging
+			StringWriter errors = new StringWriter();
+			e.printStackTrace(new PrintWriter(errors));
+			System.out.println(errors.toString());  //화면
+			log.error(errors.toString()); 		//파일+화면
+			
+			// 2. 요청구분
+			String accept = request.getHeader("accept");
+			
+			// 500 for json request
+			if(accept.matches(".*application/json.*")) {
+				// 3. json 응답
+				JsonResult jsonResult = JsonResult.fail(errors.toString());
+				String jsonString = new ObjectMapper().writeValueAsString(jsonResult);
+				
+				response.setStatus(HttpServletResponse.SC_OK);
+				response.setContentType("application/json; charset=utf-8");
+				OutputStream os = response.getOutputStream();
+				os.write(jsonString.getBytes("utf-8"));
+				os.close();
+				
+				return;
+			} 
+			
+			// 404 for html request
+			if(e instanceof NoHandlerFoundException) {
+				/*
+				 * request .getRequestDispatcher("/WEB-INF/views/errors/404.jsp")
+				 * .forward(request, response);
+				 */
+				request
+					.getRequestDispatcher("/error/404")
+					.forward(request, response);
+				return;
+			} 
+			
+			// 500 for html request
+			request.setAttribute("error", errors.toString());
+			/*
+			 * request .getRequestDispatcher("/WEB-INF/views/errors/exception.jsp")
+			 * .forward(request, response);
+			 */
+			request
+				.getRequestDispatcher("/error/500")
+				.forward(request, response);
 	}
 }
